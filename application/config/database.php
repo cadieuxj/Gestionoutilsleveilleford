58abdb667e6733ba7d62d4935770d661947535d9
cadieuxj <81915047+cadieuxj@users.noreply.github.com>
2023-11-21 09:04:41 -0500

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'stock',
+	'hostname' => 'gestionoutilsleveilleford-server.mysql.database.azure.com',
+	'username' => 'ewfogssijw',
+	'password' => 'IT3C2P181WYZBJM6$',
+	'database' => 'gestionoutilsleveilleford-database',
+	'port' => '3306',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
